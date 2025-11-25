a7670ef67ef52805b6b48e65dccccd25f2df034d
Jarcelo <jarcelo@gmail.com>
2025-11-25 14:39:23 -0400

    Update

--- admin/core/app/view/participants_list-view.php
+++ admin/core/app/view/participants_list-view.php
@@ -117,6 +117,12 @@
 
 	$(document).ready(function() {
 
+
+		toastify("Aviso: En este momento la carga está en mantenimiento", true, 25000, "warning");
+
+
+
+
 		let elementoColapsable = document.getElementById('collapseOne');
 		elementoColapsable.classList.add('hide');
 
@@ -371,7 +377,6 @@ $occupations = $con->query("SELECT * from occupations order by p_name");
 
 						<div class="card-header card-header-primary">
 							<h4 class="title text-left">Nuevo participante</h4>
-							<!-- <p class="card-category">Complete your profile</p> -->
 						</div>
 
 						<div class="col-md-12">
@@ -383,22 +388,23 @@ $occupations = $con->query("SELECT * from occupations order by p_name");
 								<div class="input-group">
 									<input type="text" class="form-control" name="buscar_participante" id="q_participante" value="" placeholder="Buscar">
 									<span class="input-group-btn">
-										<button type="button" onclick="codigoAJAX()" class="btn btn-fab btn-round btn-primary">
+										
+										<!-- <button type="button" onclick="codigoAJAX()" class="btn btn-fab btn-round btn-primary">
 											<i><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
 													<path fill="currentColor" d="m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5t1.888-4.612T9.5 3t4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3zM9.5 14q1.875 0 3.188-1.312T14 9.5t-1.312-3.187T9.5 5T6.313 6.313T5 9.5t1.313 3.188T9.5 14" />
 												</svg></i>
-										</button>
+										</button> -->
 									</span>
 								</div>
 							</div>
 						</div>
 
 
-						<div class="col-md-6">
+						<!-- <div class="col-md-6">
 							<div class="form-group">
 								<button type="button" onclick="reset_form()" class="btn btn-secundary btn-block">Crear nuevo participante si no existe</button>
 							</div>
-						</div>
+						</div> -->
 
 
 						<div class="card-body">
