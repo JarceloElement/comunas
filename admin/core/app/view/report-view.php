2a110ba685f1e8960140fb63eaf072c0975c2348
Jarcelo <jarcelo@gmail.com>
2025-12-12 17:40:50 -0400

    Update

--- admin/core/app/view/report-view.php
+++ admin/core/app/view/report-view.php
@@ -1969,9 +1969,12 @@ $DB_name = "reports";
 								$values = [$codigo_curso, (int)$total_person_fe, (int)$total_person_ma, (int)$id_activity];
 								ExecutorPg::update($sql, $values);
 
-								// $sql = "UPDATE formaciones set cod_taller=?, cod_curso=? where id_activity = ? AND cod_taller IN (SELECT codigo_taller FROM tipo_taller WHERE codigo_taller = '$codigo_taller');";
 								$sql = "UPDATE formaciones set cod_taller=? where id_activity = ? AND EXISTS (SELECT 1 FROM tipo_taller AS tt WHERE tt.codigo_taller = ?);";
 								$values = [$codigo_taller, (int)$id_activity, $codigo_taller];
+								ExecutorPg::update($sql, $values);
+
+								$sql = "UPDATE formaciones set cod_curso=? where id_activity = ? AND EXISTS (SELECT 1 FROM training_type AS tt WHERE tt.codigo_curso = ?);";
+								$values = [$codigo_curso, (int)$id_activity, $codigo_curso];
 								ExecutorPg::update($sql, $values);
 
 								// total de participantes
